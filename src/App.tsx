11c1abbe5e65da83aca605df290f45fde8d07929
z89 <n10234977@qut.edu.au>
2021-10-24 16:56:55 +1000

    success: async promise returning cid of string from go-ipfs node

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import './App.css';
+import { create } from 'ipfs-http-client'
+
+async function loadNode() {
+  // connect to the default API address http://localhost:5001
+  const client = create({host: '127.0.0.1', port: 5001})
+  let cid
+  try {
+    cid = await client.add('Hello world!')
+  } catch (err) {
+    console.log(err);
+  }
+
+  return cid
+}
+
 
 function App() {
+  loadNode().then((value) => console.log(value.path))
+  
   return (
     <div className="App">
       <h1>testing</h1>
